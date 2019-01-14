9075a9813a3b78b6dbd61b66065e7651b728ed94
Zachary New <zachary.new@pnnl.gov>
2019-01-14 13:24:49 -0800

    renamed this file

--- socialsim/measurements/infospread/infospread_measurements.py
+++
@@ -1,8 +0,0 @@
-from infospread_node       import InfospreadNode
-from infospread_population import InfospreadPopulation
-from infospread_community  import InfospreadCommunity
-
-class InfospreadMeasurements(InfospreadNode, InfospreadPopulation,
-    InfospreadCommunity):
-
-    def __init__():
