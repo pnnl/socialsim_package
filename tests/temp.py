9e18a39a905d0f2d8256bf159a5f47c752c697a1
Zachary New <zachary.new@pnnl.gov>
2019-01-14 13:26:14 -0800

    Added a few tests.

--- tests/temp.py
+++ tests/temp.py
@@ -1,16 +1,7 @@
-import sys
-import os.path
-
-socialsim_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                              os.path.pardir))
-
-sys.path.append(socialsim_path)
-
 import socialsim as ss
 
+print('socialsim imported')
 
-data_directory = 'test_datasets\\reddit\\'
+print(ss.load_data)
 
-filename = 'reddit_data_cyber_20180101_to_20180107_1week.csv'
-
-ss.csv_to_json(data_directory+filename, 'PNNL', '1', 'cyber', 'reddit')
+print(ss.TaskRunner)
