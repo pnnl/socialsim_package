bee177509d68271dbab2c61d79ec4f03fad18c74
Zachary New <zachary.new@pnnl.gov>
2019-01-18 13:38:40 -0800

    cross platform and group formation now correctly subclass the measurements baseclass.

--- socialsim/measurements/cross_platform/cross_platform.py
+++ socialsim/measurements/cross_platform/cross_platform.py
@@ -1,5 +1,7 @@
 from ..measurements import MeasurementsBaseClass
 
 class CrossPlatformMeasurements(MeasurementsBaseClass):
-    def __init__(self):
-        pass
+    def __init__(self, dataset, configuration,
+        log_file='cross_platform_measurements_log.txt'):
+        super(GroupFormationMeasurements, self).__init__(main_df, configuration,
+            log_file=log_file)
--- socialsim/measurements/group_formation/group_formation.py
+++ socialsim/measurements/group_formation/group_formation.py
@@ -1,5 +1,7 @@
 from ..measurements import MeasurementsBaseClass
 
 class GroupFormationMeasurements(MeasurementsBaseClass):
-    def __init__(self):
-        pass
+    def __init__(self, dataset, configuration,
+        log_file='group_formation_measurements_log.txt'):
+        super(CrossPlatformMeasurements, self).__init__(main_df, configuration,
+            log_file=log_file)
