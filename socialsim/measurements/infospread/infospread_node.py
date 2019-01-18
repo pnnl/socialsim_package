0f857c586495747054840439047baccf859958b0
Zachary New <zachary.new@pnnl.gov>
2019-01-18 11:03:27 -0800

    copied in lots of baseline measurements

--- socialsim/measurements/infospread/infospread_node.py
+++ socialsim/measurements/infospread/infospread_node.py
@@ -1,5 +1,15 @@
 from ..measurements import MeasurementsBaseClass
 
-class InfospreadNode(MeasurementsBaseClass):
+class InfospreadNode:
     def __init__(self):
+        """
+        Description: Intentially does nothing. Infospread parent classes are
+            just containers for functions. That is all.
+
+        Input:
+            None
+
+        Output:
+            None
+        """
         pass
--- socialsim/measurements/infospread/infospread_population.py
+++ socialsim/measurements/infospread/infospread_population.py
@@ -1,5 +1,15 @@
 from ..measurements import MeasurementsBaseClass
 
-class InfospreadPopulation(MeasurementsBaseClass):
+class InfospreadPopulation:
     def __init__(self):
+        """
+        Description: Intentially does nothing. Infospread parent classes are
+            just containers for functions. That is all.
+
+        Input:
+            None
+
+        Output:
+            None
+        """
         pass
