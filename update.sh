63ba7cc57cca867e033b6eba89c8d87b6f36d1fe
New, Zachary D <zachary.new@pnnl.gov>
2019-01-23 12:30:18 -0800

    update.sh edited online with Bitbucket

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-# This script builds a fresh distrobution of socialsim and installs it via pip.
+# This script builds a fresh distribution of socialsim and installs it via pip.
 
 python setup.py sdist
 pip install dist/socialsim-0.1.0.tar.gz
