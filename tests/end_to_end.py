3f32f2104624e2e961004ff4bc73011dee579b96
Zachary New <zachary.new@pnnl.gov>
2019-02-28 10:19:37 -0800

    fixed infospread measurements

--- tests/end_to_end.py
+++ tests/end_to_end.py
@@ -17,7 +17,9 @@ def subset_for_test(dataset, n=1000):
     return subset
 
 dataset_directory  = '/data/socialsim/dataset.txt'
-configuration_file = '/home/newz863/pic_drive/socialsim/repositories/stash/socialsim_package/tests/configuration_files/infospread.json'
+configuration_file = '/home/newz863/pic_drive/socialsim/repositories/stash/'
+configuration_file = configuration_file + 'socialsim_package/tests/'
+configuration_file = configuration_file + 'configuration_files/infospread.json'
 
 ground_truth = ss.load_data(dataset_directory, verbose=True)
 ground_truth = subset_for_test(ground_truth)
