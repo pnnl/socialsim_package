a58b951b31f5ba8b1841059a32f2830d5ca7d3bc
Zachary New <zachary.new@pnnl.gov>
2019-02-28 19:44:32 -0800

    tested socials activity measurements

---
+++ examples/social_activity.py
@@ -0,0 +1,21 @@
+import socialsim as ss
+
+# Load the example dataset
+dataset = 'example_dataset.txt'
+dataset = ss.load_data(dataset)
+
+# Subset the dataset to a particular platform 
+dataset = dataset[dataset['platform']=='github'].head(n=2000)
+
+# Load the configuration file
+config = 'example_configuration.json'
+config = ss.load_config(config)
+
+# Subset the configuration for the given task 
+config = config['github']['social_activity']
+
+# Define the measurement object
+social_activity_measurements = ss.SocialActivityMeasurements(dataset, config, None, 'github')
+
+# Run all measurements in the config file
+results = social_activity_measurements.run(verbose=True)
