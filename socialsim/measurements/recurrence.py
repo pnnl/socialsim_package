81d36c714d1783f448b726e385ec8340846ecf84
shre528 <prasha.shrestha@pnnl.gov>
2019-03-04 11:23:08 -0800

    calculate burst distribution only once

--- socialsim/measurements/recurrence.py
+++ socialsim/measurements/recurrence.py
@@ -28,6 +28,7 @@ class RecurrenceMeasurements(MeasurementsBaseClass):
         self.measurement_type = 'recurrence'
         self.get_per_epoch_counts()
         self.detect_bursts()
+        self.time_between_bursts_distribution = None
 
     def get_per_epoch_counts(self, time_granularity='Min'):
         '''
@@ -65,6 +66,12 @@ class RecurrenceMeasurements(MeasurementsBaseClass):
                 burst_interval[0], burst_interval[1], inclusive=False), 'burst_index'] = idx
         self.grouped_bursts = self.dataset_df.dropna().groupby('burst_index')
 
+    def get_time_between_bursts_distribution(self):
+        if self.time_between_bursts_distribution is None:
+            self.time_between_bursts_distribution = [(start_j - end_i).total_seconds() / (60*60*24) for (_, end_i), (start_j, _) in zip(self.burst_intervals[:-1], self.burst_intervals[1:])]
+        return self.time_between_bursts_distribution
+
+
     def number_of_bursts(self):
         '''
         How many renewed bursts of activity are there?
@@ -74,9 +81,9 @@ class RecurrenceMeasurements(MeasurementsBaseClass):
     def time_between_bursts(self):
         '''
         How much time elapses between renewed bursts of activity on average?
+        Time granularity: days
         '''
-#         print([(start_j - end_i).total_seconds() / (60*60*24) for (_, end_i), (start_j, _) in zip(self.burst_intervals[:-1], self.burst_intervals[1:])])
-        return np.mean([(start_j - end_i).total_seconds() / (60*60*24) for (_, end_i), (start_j, _) in zip(self.burst_intervals[:-1], self.burst_intervals[1:])])
+        return np.mean(self.get_time_between_bursts_distribution())
 
     def average_size_of_each_burst(self):
         '''
@@ -107,7 +114,8 @@ class RecurrenceMeasurements(MeasurementsBaseClass):
 
     def lifetime_of_each_burst(self):
         '''
-        How many minutes does each burst last on average?
+        How long does each burst last on average?
+        Time granularity: minutes
         '''
         return np.mean([(end_i - start_i).total_seconds() / 60 for (start_i, end_i) in self.burst_intervals])
 
@@ -118,11 +126,10 @@ class RecurrenceMeasurements(MeasurementsBaseClass):
         return np.mean([single_burst_df[self.platform_col].value_counts().max()/len(single_burst_df) for _, single_burst_df in self.grouped_bursts])
 
 
-
 class RecurrenceCommunityMeasurements(MeasurementsBaseClass):
     def __init__(self, dataset_df, id_col='id_h', timestamp_col="nodeTime", userid_col="nodeUserID", platform_col="platform", configuration={}, meatadata=None, communities=None, community_col="coin", log_file='recurrence_measurements_log.txt', node_list=None, community_list=None):
         """
-        :param dataset_df: dataframe containing all posts for all coins in all platforms
+        :param dataset_df: dataframe containing all posts for all communities (Eg. coins for scenario 2) in all platforms
         :param timestamp_col: name of the column containing the time of the post
         :param id_col: name of the column containing the post id
         :param userid_col: name of the column containing the user id
@@ -136,72 +143,69 @@ class RecurrenceCommunityMeasurements(MeasurementsBaseClass):
         self.community_col      = community_col
         self.platform_col       = platform_col
         self.measurement_type   = 'recurrence'
-        self.get_percoin_recurrence_measurements()
+        self.get_percommunity_recurrence_measurements()
 
-    def get_percoin_recurrence_measurements(self):
-        self.percoin_recurrence_measurements = {}
-        for coin, coin_df in self.dataset_df.groupby(self.community_col):
-            self.percoin_recurrence_measurements[coin] = RecurrenceMeasurements(dataset_df=self.dataset_df, id_col=self.id_col, timestamp_col=self.timestamp_col, userid_col=self.userid_col, platform_col=self.platform_col, configuration=self.configuration, community_col=self.community_col)
+    def get_percommunity_recurrence_measurements(self):
+        self.percommunity_recurrence_measurements = {}
+        for community, community_df in self.dataset_df.groupby(self.community_col):
+            self.percommunity_recurrence_measurements[community] = RecurrenceMeasurements(dataset_df=self.dataset_df, id_col=self.id_col, timestamp_col=self.timestamp_col, userid_col=self.userid_col, platform_col=self.platform_col, configuration=self.configuration, community_col=self.community_col)
 
-    def run_for_all_coins(self, measurement_name):
-        return [getattr(percoin_recurrence_measurements, measurement_name)() for coin, percoin_recurrence_measurements in self.percoin_recurrence_measurements.items()]
-        # for coin, percoin_recurrence_measurements in self.percoin_recurrence_measurements.items():
-        #     to_run = getattr(percoin_recurrence_measurements, measurement_name)()
-        #     print(to_run)
+    def run_for_all_communities(self, measurement_name):
+        return [getattr(percommunity_recurrence_measurements, measurement_name)() for community, percommunity_recurrence_measurements in self.percommunity_recurrence_measurements.items()]
 
 
     def distribution_of_number_of_bursts(self):
         '''
         How does the number of renewed bursts of activity vary across different pieces of information?
         '''
-        return self.run_for_all_coins('number_of_bursts')
+        return self.run_for_all_communities('number_of_bursts')
 
 
     def distribution_of_time_between_bursts(self):
         '''
         How does the time elapsed between renewed bursts of activity vary across different pieces of information?
         '''
-        return self.run_for_all_coins('time_between_bursts')
+        return self.run_for_all_communities('time_between_bursts')
 
 
     def distribution_of_average_burst_size(self):
         '''
         How does the number times is the information shared per burst vary across different pieces of information?
         '''
-        return self.run_for_all_coins('average_size_of_each_burst')
+        return self.run_for_all_communities('average_size_of_each_burst')
 
 
     def distribution_of_average_number_of_users_per_burst(self):
         '''
         How does the number of users reached during each burst vary across different pieces of information?
         '''
-        return self.run_for_all_coins('average_number_of_users_per_burst')
+        return self.run_for_all_communities('average_number_of_users_per_burst')
 
 
     def distribution_of_burst_timing_burstiness(self):
         '''
         How does the burstiness of burst timing vary across different pieces of information?
         '''
-        return self.run_for_all_coins('number_of_bursts')
+        return self.run_for_all_communities('number_of_bursts')
 
 
     def distribution_of_new_users_per_burst(self):
         '''
         How does the number of new users reached during each burst vary across different pieces of information?
         '''
-        return self.run_for_all_coins('new_users_per_burst')
+        return self.run_for_all_communities('new_users_per_burst')
 
 
     def distribution_of_burst_lifetime(self):
         '''
         How does the burst length vary across different pieces of information?
         '''
-        return self.run_for_all_coins('lifetime_of_each_burst')
+        return self.run_for_all_communities('lifetime_of_each_burst')
 
 
     def distribution_of_burst_platform_proportion(self):
         '''
         How does the prominence of a single platform vary across different pieces of information?
         '''
-        return self.run_for_all_coins('average_proportion_of_top_platform_per_burst')
+        return self.run_for_all_communities('average_proportion_of_top_platform_per_burst')
 
